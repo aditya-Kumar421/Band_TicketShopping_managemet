a36b383a372e89968b57a8b6f43698cf0abe212d
Aditya <ak.adityakumar421@gmail.com>
2023-12-12 21:20:05 +0530

    add login and signup

--- Indexscript.js
+++ Indexscript.js
@@ -1,20 +1,23 @@
-// script.js
-
 const users = {}; // Simulated user database
 
 document.getElementById('signupForm').addEventListener('submit', function(event) {
   event.preventDefault();
   const username = document.getElementById('signupUsername').value;
+  const email = document.getElementById('signupEmail').value;
   const password = document.getElementById('signupPassword').value;
 
   // Simulating hash-based authentication (not secure for real use)
   const hashedPassword = hashPassword(password);
 
-  users[username] = { password: hashedPassword };
+  // Storing user information securely in the database
+  users[username] = {
+    email: email,
+    password: hashedPassword
+  };
   alert('Signup successful!');
   console.log(users);
 
-  // Redirect to index.html after signup
+  // Redirect to login.html after signup
   window.location.href = 'index.html';
 });
 
@@ -26,9 +29,10 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
   // Simulating hash-based authentication (not secure for real use)
   const hashedPassword = hashPassword(password);
 
+  // Checking if the user exists and the password matches
   if (users[username] && users[username].password === hashedPassword) {
     alert('Login successful! Redirecting...');
-    
+
     // Redirect to index.html after login
     window.location.href = 'index.html';
   } else {
@@ -38,7 +42,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
 // Simulating hash function
 function hashPassword(password) {
-  // Simulated hashing using a simple approach (not secure)
+  // Simulated hashing using a more secure approach (not fully secure)
   let hash = 0;
   if (password.length === 0) {
     return hash;
